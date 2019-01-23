5008e851b97ab2a17610b772fa9b7ac3e73ba78d
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-23 13:48:30 -0700

    set up axios request

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    friendsList: [],
+    error: ''
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/friends")
+      .then(res => {
+        console.log(res.data);
+        this.setState({ 
+          friendsList: res,
+          error: ''
+        });
+      })
+      .catch(err => {
+        console.log(err.response.data);
+        this.setState({error: err.response.data});
+      });
+  }
+
   render() {
     return (
       <div className="App">
